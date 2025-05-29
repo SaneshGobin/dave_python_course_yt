75fa7b2c87b2a49d4c3b0c72dee74c1becfe08e2
Sanesh Gobin <126343205+SaneshGobin@users.noreply.github.com>
2025-05-29 09:51:32 +0200

    Add Lesson_three folder and complete lesson

---
+++ Lesson_three/main.py
@@ -0,0 +1,18 @@
+# Boolean operators because they are operating(produce a boolean value) on boolean value(after evaluating the value true or false)'
+# and, or, not
+
+
+# Comparison operators because operating/comparing equality on values
+# <, >, ==, <=, >=
+
+
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10 else print('Not today')
